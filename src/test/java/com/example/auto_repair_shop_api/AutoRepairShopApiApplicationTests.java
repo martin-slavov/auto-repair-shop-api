3f6bc103899e35b6d9fd46d3487272726fe231bc
martin-slavov <mstefslav@gmail.com>
2026-09-07 11:30:08 +0300

    Initial commit

---
+++ src/test/java/com/example/auto_repair_shop_api/AutoRepairShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auto_repair_shop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoRepairShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
